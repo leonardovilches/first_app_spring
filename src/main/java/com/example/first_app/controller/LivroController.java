3a3ffe630841069e690add1edcadc24158e0dcca
Leonardo Vilches <leovilches08@gmail.com>
2019-12-10 18:54:51 -0300

    first commit

---
+++ src/main/java/com/example/first_app/controller/LivroController.java
@@ -0,0 +1,41 @@
+package com.example.first_app.controller;
+
+
+import com.example.first_app.repository.LivroRepository;
+import com.example.first_app.entity.Livro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class LivroController {
+
+    private LivroRepository livroRepository;
+
+    @Autowired
+    public LivroController(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String listaLivros(@PathVariable("autor") String autor, Model model) {
+        List<Livro> listaLivros = livroRepository.findByAutor(autor);
+        if (listaLivros != null) {
+            model.addAttribute("livros", listaLivros);
+        }
+        return "listaLivros";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String adicionaLivroAutor(@PathVariable("autor") String autor, Livro livro) {
+        livro.setAutor(autor);
+        livroRepository.save(livro);
+        return "redirect:/";
+    }
+}
